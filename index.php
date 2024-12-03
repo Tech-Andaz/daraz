7cfdb5f9b7068ee225489291e7d06c0e2633bcb5
daiumahsanbutt <32085886+daiumahsanbutt@users.noreply.github.com>
2024-12-03 18:37:14 +0500

    Tickets

--- index.php
+++ index.php
@@ -6,12 +6,9 @@ use TechAndaz\Daraz\DarazClient;
 use TechAndaz\Daraz\DarazAPI;
 
 //Test Account
-// $DarazClient = new DarazClient("502736", "GapPqYo58gd8bQlVX8OtY9gnrwvmgY5Q", "https://portal.alfatah.pk/integration/daraz", "50000600223xLOTpBFSzerjMIq0QUfZ5mxe13da99a0pqcupjWkEw6NS3Xu5Pk");
-//Alfatah Account
-$DarazClient = new DarazClient("502736", "GapPqYo58gd8bQlVX8OtY9gnrwvmgY5Q", "https://portal.alfatah.pk/integration/daraz", "50000602033YO3q5r0ThJHLmWHyxlG0HovjMoyxtRX29o13e2b163gIFJYufRu");
+$DarazClient = new DarazClient("502736", "GapPqYo58gd8bQlVX8OtY9gnrwvmgY5Q", "https://portal.alfatah.pk/integration/daraz", "50000600223xLOTpBFSzerjMIq0QUfZ5mxe13da99a0pqcupjWkEw6NS3Xu5Pk");
 $DarazAPI = new DarazAPI($DarazClient);
 
-//Generate Authorization URL
 function generateSellerAuthURL($DarazAPI){
     try {
         $response = $DarazAPI->generateSellerAuthURL();
@@ -20,7 +17,6 @@ function generateSellerAuthURL($DarazAPI){
         echo "Error: " . $e->getMessage() . "\n";
     }
 }
-//Exchange Auth Code for Access Token
 function exchangeCodeForToken($DarazAPI){
     try {
         $code = "4_502736_ByjGgqGLgnPYep6atK2SGmnd1062";
@@ -30,7 +26,6 @@ function exchangeCodeForToken($DarazAPI){
         echo "Error: " . $e->getMessage() . "\n";
     }
 }
-//Refresh Access Token
 function refreshAccessToken($DarazAPI){
     try {
         $refresh_token = "50001600123bi1wUPEDULeTlqCsZRvaf5jv18b72f69kiscsvcfFhsgL1or7jN";
@@ -40,129 +35,85 @@ function refreshAccessToken($DarazAPI){
         echo "Error: " . $e->getMessage() . "\n";
     }
 }
-//Make Request
-function makeRequest($DarazAPI){
+function getAllOrders($DarazAPI){
     try {
         $settings = array(
-            "endpoint" => "/orders/get",
-            "method" => "GET",
             "parameters" => array(
                 "sort_by" => "created_at",
                 "sort_direction" => "DESC",
                 "created_after" => "2017-02-10T09:00:00+08:00"
             ),
         );
-        $response = $DarazAPI->makeRequest($settings);
+        $response = $DarazAPI->getAllOrders($settings);
         return $response;
     } catch (TechAndaz\Daraz\DarazException $e) {
         echo "Error: " . $e->getMessage() . "\n";
     }
 }
-function makeRequest3($DarazAPI, $order_id){
+function getOrder($DarazAPI, $order_id){
     try {
-        $settings = array(
-            "endpoint" => "/order/get",
-            "method" => "GET",
-            "parameters" => array(
-                "order_id" => "$order_id",
-            ),
-        );
-        $response = $DarazAPI->makeRequest($settings);
+        $response = $DarazAPI->getOrder($order_id);
         return $response;
     } catch (TechAndaz\Daraz\DarazException $e) {
         echo "Error: " . $e->getMessage() . "\n";
     }
 }
-//Make Request 2
-function makeRequest2($DarazAPI){
+function getOrderItems($DarazAPI, $order_id){
     try {
-        $settings = array(
-            "endpoint" => "/order/items/get",
-            "method" => "GET",
-            "parameters" => array(
-                "order_id" => "127242507235768"
-            ),
-        );
-        $response = $DarazAPI->makeRequest($settings);
+        $response = $DarazAPI->getOrderItems($order_id);
         return $response;
     } catch (TechAndaz\Daraz\DarazException $e) {
         echo "Error: " . $e->getMessage() . "\n";
     }
 }
-
-function convertOrderToShopify($DarazAPI){
-    $order_number = "203082453807589";
-    $order = json_decode(makeRequest3($DarazAPI, $order_number),true)['data'];
-    $order_number = $order['order_number'];
-
-    $settings = array(
-        "endpoint" => "/order/items/get",
-        "method" => "GET",
-        "parameters" => array(
-            "order_id" => "$order_number"
-        ),
-    );
-    $order_items = json_decode($DarazAPI->makeRequest($settings),true)['data'];
-
-    $randomKeys = array_rand($order_items, 4);
-    $randomSubset = is_array($randomKeys) ? array_intersect_key($order_items, array_flip($randomKeys)) : [$order_items[$randomKeys]];
-
-    packOrder($DarazAPI, $order['order_id'], array_column($randomSubset,"order_item_id"));
-
+function fetchAndShipOrder($DarazAPI){
+    $order = getOrder($DarazAPI, "203082453807589")['data'];
+    $order_items = getOrderItems($DarazAPI, $order['order_number'])['data'];
+    $response = packAndShipOrder($DarazAPI, $order['order_id'], array_column($order_items, "order_item_id"));
+    print_r($response);
+    exit;
 }
-
 function packOrder($DarazAPI, $order_id, $items){
-    $settings = array(
-        "endpoint" => "/order/fulfill/pack",
-        "method" => "POST",
-        "parameters" => array(
-            "packReq" => json_encode(array(
-                "pack_order_list" => array(
-                    array(
-                        "order_item_list" => $items,
-                        "order_id" => "$order_id",
-                    ),
-                ),
-                "shipping_allocate_type" => "TFS",
-                "delivery_type" => "dropship",
-            ))
-        ),
-    );
-    $pack = json_decode($DarazAPI->makeRequest($settings),true);
-    if($pack['code'] == 0 && isset($pack['result']['data'])){
-        $data = $pack['result']['data']['pack_order_list'][0]['order_item_list'];
-        $packgaes = array_column($data, "package_id");
-        print_r($packgaes);
-        printLabel($DarazAPI, $packgaes);
+    try {
+        $settings = array(
+            "order_id" => "$order_id",
+            "items" => $items,
+        );
+        $response = $DarazAPI->packOrderItems($settings);
+        return $response;
+    } catch (TechAndaz\Daraz\DarazException $e) {
+        echo "Error: " . $e->getMessage() . "\n";
     }
 }
 function printLabel($DarazAPI, $packages){
-    $packages = array_unique($packages);
-    $convertedArray = array_map(function($packageId) {
-        return ["package_id" => $packageId];
-    }, $packages);
-
-    
-    $settings = array(
-        "endpoint" => "/order/package/document/get",
-        "method" => "POST",
-        "parameters" => array(
-            "getDocumentReq" => json_encode(array(
-                "packages" => $convertedArray,
-                "doc_type" => "PDF",
-                "print_item_list" => true,
-            ))
-        ),
-    );
-    print_r($settings);
-    $label = json_decode($DarazAPI->makeRequest($settings),true);
-    print_r($label);
+    try {
+        $settings = array(
+            "packages" => $packages,
+        );
+        $response = $DarazAPI->printShippingLabel($settings);
+        return $response;
+    } catch (TechAndaz\Daraz\DarazException $e) {
+        echo "Error: " . $e->getMessage() . "\n";
+    }
 }
-// echo (generateSellerAuthURL($DarazAPI));
-// echo (exchangeCodeForToken($DarazAPI));
-// echo (refreshAccessToken($DarazAPI));
-// echo (makeRequest($DarazAPI));
-// echo (makeRequest2($DarazAPI));
-echo (convertOrderToShopify($DarazAPI));
-// echo json_encode(getPickupLocation($DarazAPI));
+function packAndShipOrder($DarazAPI, $order_id, $items){
+    try {
+        $settings = array(
+            "order_id" => "$order_id",
+            "items" => $items,
+        );
+        $response = $DarazAPI->packAndShipOrder($settings);
+        return $response;
+    } catch (TechAndaz\Daraz\DarazException $e) {
+        echo "Error: " . $e->getMessage() . "\n";
+    }
+}
+// print_r (generateSellerAuthURL($DarazAPI));
+// print_r (exchangeCodeForToken($DarazAPI));
+// print_r (refreshAccessToken($DarazAPI));
+// print_r (getAllOrders($DarazAPI));
+// print_r (getOrder($DarazAPI, "203082453807589"));
+// print_r (getOrderItems($DarazAPI, "203082453807589"));
+// print_r (fetchAndShipOrder($DarazAPI));
+// print_r json_encode(getPickupLocation($DarazAPI));
 ?>
--- src/Daraz/DarazAPI.php
+++ src/Daraz/DarazAPI.php
@@ -35,4 +35,169 @@ class DarazAPI
     public function makeRequest($settings){
         return $this->DarazClient->makeRequest($settings);
     }
+    public function getAllOrders($config){
+        $parameters = isset($config['parameters']) ? $config['parameters'] : throw new DarazException('Parameters are required');
+        $parameters = is_array($config['parameters']) ? $config['parameters'] : throw new DarazException('Parameters must be an array');
+        $parameters = array_unique($parameters);
+        if(count($parameters) == 0){
+            throw new DarazException('Must have atleast 1 parameter');
+        }
+        $settings = array(
+            "endpoint" => "/orders/get",
+            "method" => "GET",
+            "parameters" => $parameters,
+        );
+        $response = json_decode($this->DarazClient->makeRequest($settings),true);
+        if(!isset($response['data']['orders'])){
+            return array(
+                "status" => 0,
+                "error" => "There was an error processing request",
+                "response" => $response
+            );
+        } else {
+            return array(
+                "status" => 1,
+                "data" => $response['data']
+            );
+        }
+    }
+    public function getOrder($order_id){
+        $order_id = isset($order_id) ? $order_id : throw new DarazException('Order ID is required');
+        $settings = array(
+            "endpoint" => "/order/get",
+            "method" => "GET",
+            "parameters" => array(
+                "order_id" => "$order_id",
+            ),
+        );
+        $response = json_decode($this->DarazClient->makeRequest($settings),true);
+        if(!isset($response['data'])){
+            return array(
+                "status" => 0,
+                "error" => "There was an error processing request",
+                "response" => $response
+            );
+        } else {
+            return array(
+                "status" => 1,
+                "data" => $response['data']
+            );
+        }
+    }
+    public function getOrderItems($order_id){
+        $order_id = isset($order_id) ? $order_id : throw new DarazException('Order ID is required');
+        $settings = array(
+            "endpoint" => "/order/items/get",
+            "method" => "GET",
+            "parameters" => array(
+                "order_id" => "$order_id",
+            ),
+        );
+        $response = json_decode($this->DarazClient->makeRequest($settings),true);
+        if(!isset($response['data'])){
+            return array(
+                "status" => 0,
+                "error" => "There was an error processing request",
+                "response" => $response
+            );
+        } else {
+            return array(
+                "status" => 1,
+                "data" => $response['data']
+            );
+        }
+    }
+    public function packOrderItems($config){
+        $order_id = isset($config['order_id']) ? $config['order_id'] : throw new DarazException('Order ID is required');
+        $items = isset($config['items']) ? $config['items'] : throw new DarazException('Items are required');
+        $items = is_array($config['items']) ? $config['items'] : throw new DarazException('Items must be an array');
+        $items = array_unique($items);
+        if(count($items) == 0){
+            throw new DarazException('Must have atleast 1 item');
+        }
+        $shipping_allocate_type = isset($config['shipping_allocate_type']) ? $config['shipping_allocate_type'] : "TFS";
+        $delivery_type = isset($config['delivery_type']) ? $config['delivery_type'] : "dropship";
+        $settings = array(
+            "endpoint" => "/order/fulfill/pack",
+            "method" => "POST",
+            "parameters" => array(
+                "packReq" => json_encode(array(
+                    "pack_order_list" => array(
+                        array(
+                            "order_item_list" => $items,
+                            "order_id" => "$order_id",
+                        ),
+                    ),
+                    "shipping_allocate_type" => "$shipping_allocate_type",
+                    "delivery_type" => "$delivery_type",
+                ))
+            ),
+        );
+        $response = json_decode($this->DarazClient->makeRequest($settings),true);
+        if($response['result']['success'] != 1){
+            return array(
+                "status" => 0,
+                "error" => "There was an error processing request",
+                "response" => $response
+            );
+        } else {
+            $data = $response['result']['data']['pack_order_list'][0]['order_item_list'];
+            $packages = array_column($data, "package_id");
+            return array(
+                "status" => 1,
+                "packages" => $packages
+            );
+        }
+    }
+    public function printShippingLabel($config){
+        $packages = isset($config['packages']) ? $config['packages'] : throw new DarazException('Packages are required');
+        $packages = is_array($config['packages']) ? $config['packages'] : throw new DarazException('Packages must be an array');
+        $packages = array_unique($packages);
+        $packages = array_map(function($packageId) {
+            return ["package_id" => $packageId];
+        }, $packages);
+        if(count($packages) == 0){
+            throw new DarazException('Must have atleast 1 package');
+        }
+        $print_item_list = isset($config['print_item_list']) ? $config['print_item_list'] : true;
+        $doc_type = isset($config['doc_type']) ? $config['doc_type'] : "PDF";
+        $settings = array(
+            "endpoint" => "/order/package/document/get",
+            "method" => "POST",
+            "parameters" => array(
+                "getDocumentReq" => json_encode(array(
+                    "packages" => $packages,
+                    "doc_type" => "$doc_type",
+                    "print_item_list" => $print_item_list,
+                ))
+            ),
+        );
+        $response = json_decode($this->DarazClient->makeRequest($settings),true);
+        if($response['result']['success'] != 1){
+            return array(
+                "status" => 0,
+                "error" => "There was an error processing request",
+                "response" => $response
+            );
+        } else {
+            $data = $response['result']['data'];
+            return array(
+                "status" => 1,
+                "label_url" => $data['pdf_url'],
+                "response" => $data
+            );
+        }
+    }
+    public function packAndShipOrder($config){
+        $response = $this->packOrderItems($config);
+        if($response['status'] == 0){
+            return $response;
+        }
+        $packages = $response['packages'];
+        $settings = array(
+            "packages" => $packages,
+        );
+        $response = $this->printShippingLabel($settings);
+        return $response;
+    }
 }
